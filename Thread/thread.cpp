2302e843312d6fd151bb5ced18fbf9ccadec7723
KIRA <1033956371@qq.com>
2016-01-14 01:55:16 +0800

    [task] add Thread DIR.

---
+++ Thread/thread.cpp
@@ -0,0 +1,18 @@
+#include<pthread.h>
+
+//int pthread_create(thread_t	*thread,
+//					const pthread_attr_t*  attr,
+//					void*(*start_routine)(void*),
+//					void* arg);
+//
+//pthread_self()
+//
+//pthread_join()
+//
+//pthread_exit()
+//
+int main()
+{
+
+return 0;
+}
